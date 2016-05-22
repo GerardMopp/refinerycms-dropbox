71c5557ee07ff263fdcb644a1c3d048e5dd01f35
Gerard Mopp <gerardmopp@gmail.com>
2016-05-22 11:12:27 +0200

    inital commit

---
+++ lib/refinerycms/dropbox.rb
@@ -0,0 +1,7 @@
+require "refinerycms/dropbox/version"
+
+module Refinerycms
+  module Dropbox
+    # Your code goes here...
+  end
+end
---
+++ lib/refinerycms/dropbox/version.rb
@@ -0,0 +1,5 @@
+module Refinerycms
+  module Dropbox
+    VERSION = "0.1.0"
+  end
+end
